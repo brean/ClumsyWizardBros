141c53cbfa609dbd231cc324f7ffbec8982a6a07
Andreas Bresser <self@andreasbresser.de>
2018-01-28 21:01:54 +0100

    cleanup for movement code

--- Assets/Cards/Scripts/CardManager.cs
+++ Assets/Cards/Scripts/CardManager.cs
@@ -70,85 +70,40 @@ public class CardManager : MonoBehaviour {
 
     private void Update()
     {
+        List<Card> cards = CardSlots[0].GetCardsFromTransform();
+        Card middleCard = cards[1];
+        Card leftCard = cards[0];
+        Card rightCard = cards[2];
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            List<Card> cards = CardSlots[0].GetCardsFromTransform();
-            Card firstCard = cards[0];
-            foreach (Card card in cards)
-            {
-                if (card.transform.position.x < firstCard.transform.position.x)
-                {
-                    firstCard = card;
-                }
-            }
-            firstCard.GetComponent<Toggle>().isOn = true;
+            leftCard.GetComponent<Toggle>().isOn = true;
         }
 		else if (Input.GetKeyDown(KeyCode.Alpha2))
 		{
-			List<Card> cards = CardSlots[0].GetCardsFromTransform();
-			Card middleCard = cards[0];
-			Card leftCard = cards [1];
-			Card rightCard = cards [2];
-			foreach (Card card in cards)
-			{
-				if ((card.transform.position.x < leftCard.transform.position.x && card.transform.position.x > rightCard.transform.position.x)||(card.transform.position.x > leftCard.transform.position.x && card.transform.position.x < rightCard.transform.position.x)){
-					middleCard = card;
-				}
-			}
 			middleCard.GetComponent<Toggle>().isOn = true;
 		}
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            List<Card> cards = CardSlots[0].GetCardsFromTransform();
-            Card lastCard = cards[0];
-            foreach (Card card in cards)
-            {
-                if (card.transform.position.x > lastCard.transform.position.x)
-                {
-                    lastCard = card;
-                }
-            }
-            lastCard.GetComponent<Toggle>().isOn = true;
+            rightCard.GetComponent<Toggle>().isOn = true;
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha8))
+
+        cards = CardSlots[1].GetCardsFromTransform();
+        middleCard = cards[1];
+        leftCard = cards[0];
+        rightCard = cards[2];
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
         {
-            List<Card> cards = CardSlots[1].GetCardsFromTransform();
-            Card firstCard = cards[0];
-            foreach (Card card in cards)
-            {
-                if (card.transform.position.x < firstCard.transform.position.x)
-                {
-                    firstCard = card;
-                }
-            }
-            firstCard.GetComponent<Toggle>().isOn = true;
+            leftCard.GetComponent<Toggle>().isOn = true;
         }
 		else if (Input.GetKeyDown(KeyCode.Alpha9))
 		{
-			List<Card> cards = CardSlots[0].GetCardsFromTransform();
-			Card middleCard = cards[0];
-			Card leftCard = cards [1];
-			Card rightCard = cards [2];
-			foreach (Card card in cards)
-			{
-				if ((card.transform.position.x < leftCard.transform.position.x && card.transform.position.x > rightCard.transform.position.x)||(card.transform.position.x > leftCard.transform.position.x && card.transform.position.x < rightCard.transform.position.x)){
-					middleCard = card;
-				}
-			}
 			middleCard.GetComponent<Toggle>().isOn = true;
 		}
         else if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            List<Card> cards = CardSlots[1].GetCardsFromTransform();
-            Card lastCard = cards[0];
-            foreach (Card card in cards)
-            {
-                if (card.transform.position.x > lastCard.transform.position.x)
-                {
-                    lastCard = card;
-                }
-            }
-            lastCard.GetComponent<Toggle>().isOn = true;
+            rightCard.GetComponent<Toggle>().isOn = true;
         }
     }
 
