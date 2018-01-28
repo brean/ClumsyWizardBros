63cf68d96f3c43daffa1dffd2ba407b354e0c503
Andreas Bresser <self@andreasbresser.de>
2018-01-28 15:15:25 +0100

    try to reposition, fix for fading script (just shows a warning now instead of infinite errors)

--- Assets/Scripts/MenuScene.cs
+++ Assets/Scripts/MenuScene.cs
@@ -6,21 +6,49 @@ public class MenuScene : MonoBehaviour {
 	private CanvasGroup fadeGroup;
 	private float fadeInSpeed = 0.33f;
 
+    /*
+     * TODO:
+    private CanvasGroup FindCanvasGroup(Transform parent)
+    {
+        CanvasGroup grp = parent.GetComponent<CanvasGroup>();
+        if (grp != null)
+        {
+            return grp;
+        }
+        foreach (Transform child in parent)
+        {
+            grp = FindCanvasGroup(child);
+            if (child != null)
+            {
+                return grp;
+            }
+        }
+        return grp;
+    }
+    */
 
 	// Use this for initialization
 	private void Start () {
-		//Grab the only CanvasGroup in the scene - would lead to trouble if we would have more
-		fadeGroup = FindObjectOfType<CanvasGroup>();
-
-		//Start with white screen
-		fadeGroup.alpha = 1;
+        //Grab the only CanvasGroup in the scene - would lead to trouble if we would have more
+        //fadeGroup = FindCanvasGroup(transform);
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+        if (fadeGroup == null)
+        {
+            Debug.LogWarning("can not find CanvasGroup (for fading)!");
+        }
+        else
+        {
+            //Start with white screen
+            fadeGroup.alpha = 1;
+        }
 
 	}
 
 	// Update is called once per frame
 	private void Update () {
 		//Fade-In
-		fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+        if (fadeGroup != null)
+		    fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
 	}
 	
 }
