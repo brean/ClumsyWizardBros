a7c77cbc3cdce21ec8f1d67dd6325b24099baf21
Andreas Bresser <self@andreasbresser.de>
2018-01-28 15:57:01 +0100

    Show swap animation

---
+++ Assets/Scripts/SwapAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapAnimation : MonoBehaviour {
+    public int fps = 2;
+    private int pos = 0;
+    private float lastTime;
+
+    public List<Sprite> sprites;
+
+
+	// Use this for initialization
+	void Start () {
+        lastTime = Time.time;
+	}
+
+    void switchImage()
+    {
+        pos++;
+        GetComponent<SpriteRenderer>().sprite = sprites[pos % sprites.Count];
+    }
+
+    public void disableMe()
+    {
+        gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - lastTime > 1.0/fps)
+        {
+            switchImage();
+            lastTime = Time.time;
+        }
+	}
+}
